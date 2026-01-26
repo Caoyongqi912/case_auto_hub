d158991addb055310613e5d9d9fa596ed908840a
cyq <caoyongqi@5i5j.com>
2026-01-26 13:31:18 +0800

    优化

--- app/controller/caseHub/mind_case.py
+++ app/controller/caseHub/mind_case.py
@@ -4,7 +4,7 @@ from app.controller import Authentication
 from app.mapper.caseHub.mindcaseMapper import MindCaseMapper
 from app.model.base import User
 from app.response import Response
-from app.schema.hub.testCaseSchema import InsertMindCaseSchema
+from app.schema.hub.testCaseSchema import InsertMindCaseSchema, UpdateMindCaseSchema
 
 router = APIRouter(prefix="/hub/mindCase", tags=['用例'])
 
@@ -18,7 +18,16 @@ async def insert_case(data: InsertMindCaseSchema, user: User = Depends(Authentic
     return Response.success(data)
 
 
+@router.post("/update", description="添加用例")
+async def update_case(data: UpdateMindCaseSchema, user: User = Depends(Authentication())):
+    data = await MindCaseMapper.update_by_id(
+        updateUser=user,
+        **data.model_dump(exclude_none=True)
+    )
+    return Response.success(data)
+
+
 @router.get("/detail", description="添加用例")
-async def case_detail(case_id: int, _: User = Depends(Authentication())):
-    data = await MindCaseMapper.get_by_id(case_id)
+async def case_detail(requirement_id: int, _: User = Depends(Authentication())):
+    data = await MindCaseMapper.get_by(requirement_id=requirement_id)
     return Response.success(data)
